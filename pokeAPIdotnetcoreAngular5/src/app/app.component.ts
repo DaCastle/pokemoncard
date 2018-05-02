a9a1b2bf0480ac35c8ef4c78c58c3417a2267e9a
Dakota Castleberg <DCastleberg@rbaconsulting.com>
2018-05-02 10:52:39 -0500

    no-bitwise error for tslint and double quotes to single quotes

--- pokeAPIdotnetcoreAngular5/src/app/app.component.ts
+++ pokeAPIdotnetcoreAngular5/src/app/app.component.ts
@@ -13,33 +13,33 @@ export class AppComponent implements OnInit {
   abilities: string[] = [];
   stats: string[] = [];
   types: string[] = [];
-  cardColor: string = "#ffffff";
-  mainSpriteBackgroundColor: string = "#ffffff";
-  catGif: string = "/assets/images/cat.gif";
+  cardColor = '#ffffff';
+  mainSpriteBackgroundColor = '#ffffff';
+  catGif = '/assets/images/cat.gif';
   mainSprite: string = this.catGif;
   prev: string = this.catGif;
   next: string = this.catGif;
-  searchValue: string = "";
+  searchValue = '';
 
   typeColors: {} = {
-    "normal": "A8A77A",
-    "bug": "A6B91A",
-    "grass": "7AC74C",
-    "fire": "EE8130",
-    "water": "6390F0",
-    "ice": "96D9D6",
-    "electric": "F7D02C",
-    "psychic": "F95587",
-    "ghost": "735797",
-    "fighting": "C22E28",
-    "poison": "A33EA1",
-    "ground": "E2BF65",
-    "flying": "A98FF3",
-    "rock": "B6A136",
-    "dragon": "6F35FC",
-    "dark": "705746",
-    "steel": "B7B7CE",
-    "fairy": "D685AD",
+    'normal': 'A8A77A',
+    'bug': 'A6B91A',
+    'grass': '7AC74C',
+    'fire': 'EE8130',
+    'water': '6390F0',
+    'ice': '96D9D6',
+    'electric': 'F7D02C',
+    'psychic': 'F95587',
+    'ghost': '735797',
+    'fighting': 'C22E28',
+    'poison': 'A33EA1',
+    'ground': 'E2BF65',
+    'flying': 'A98FF3',
+    'rock': 'B6A136',
+    'dragon': '6F35FC',
+    'dark': '705746',
+    'steel': 'B7B7CE',
+    'fairy': 'D685AD',
   };
 
 
@@ -80,25 +80,25 @@ export class AppComponent implements OnInit {
 
   getNextSprite(id): void {
     if (id <= 801) {
-      this.getSpriteUrl("next", id + 1);
+      this.getSpriteUrl('next', id + 1);
     } else {
-      this.getSpriteUrl("next", 1);
+      this.getSpriteUrl('next', 1);
     }
-  }  
+  }
 
   getPrevSprite(id): void {
     if (id >= 2) {
-      this.getSpriteUrl("prev", id - 1);
+      this.getSpriteUrl('prev', id - 1);
     } else {
-      this.getSpriteUrl("prev", 802);
+      this.getSpriteUrl('prev', 802);
     }
   }
 
   getCardColor(type): string {
-    let color = "#ffffff";
+    let color = '#ffffff';
     Object.keys(this.typeColors).forEach(listType => {
       if (type === listType) {
-        color = "#" + this.typeColors[listType];
+        color = '#' + this.typeColors[listType];
       }
     });
     color = this.hexToRgbA(color);
@@ -107,19 +107,19 @@ export class AppComponent implements OnInit {
 
   dataSetter(data): void {
     this.pokemon = data;
-    this.getSpriteUrl("mainSprite", this.pokemon.id);
+    this.getSpriteUrl('mainSprite', this.pokemon.id);
     this.abilities = this.pokemon.abilities;
     this.stats = this.pokemon.stats;
     this.types = this.pokemon.types;
-    this.cardColor = this.getCardColor(this.types[this.types.length - 1]["type"]["name"]);
-    this.mainSpriteBackgroundColor = this.cardColor.replace(",.51)", ",1)");
+    this.cardColor = this.getCardColor(this.types[this.types.length - 1]['type']['name']);
+    this.mainSpriteBackgroundColor = this.cardColor.replace(',.51)', ',1)');
   }
 
   hexToRgbA(hex): string {
-  var c;
+  let c;
   if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
     c = hex.substring(1).split('');
-    if (c.length == 3) {
+    if (c.length === 3) {
       c = [c[0], c[0], c[1], c[1], c[2], c[2]];
     }
     c = '0x' + c.join('');
